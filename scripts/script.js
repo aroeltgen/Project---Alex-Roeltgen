e11d37a86f893c930bd3cd2500ff8ade14d980c5
aroeltgen <158352660+aroeltgen@users.noreply.github.com>
2024-05-02 13:40:17 -0400

    update

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+ Author: Alexander Roeltgen
+ Date: april 24
+ File Name: script.js
+ */
+
+ //Hamburger menu function
+ function hamburger() {
+ var menu = document.getElementById("menu-links");
+ if (menu.style.display === "block") {
+ menu.style.display = "none";
+ } else {
+ menu.style.display = "block";
+ }
+ }
